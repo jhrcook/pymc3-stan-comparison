2f8543e89094209ef52b6fa308a965967c90fa0a
Josh Cook <joshuacook0023@gmail.com>
2021-11-15 16:57:23 -0500

    feat: script to run pipeline

---
+++ run-pipeline.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --account=park
+#SBATCH -c 2
+#SBATCH -p priority
+#SBATCH -t 0-01:00
+#SBATCH --mem 10G
+#SBATCH -o logs/%j_pipeline.log
+#SBATCH -e logs/%j_pipeline.log
+
+module unload python
+module load gcc conda2 slurm-drmaa/1.1.1
+
+# shellcheck source=/dev/null
+source "$HOME/.bashrc"
+conda activate ppl-comp-smk
+
+snakemake \
+    --cores 1 \
+    --restart-times 0 \
+    --latency-wait 120 \
+    --use-conda \
+    --keep-going \
+    --printshellcmds
+
+conda deactivate
+exit 0
