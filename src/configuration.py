6a60d46e9c78e6906de4ee60421cb6b7578c178c
Josh Cook <joshuacook0023@gmail.com>
2021-11-18 17:43:10 -0500

    refactor: swap if-else and asset_never pattern to lookup dict

--- src/configuration.py
+++ src/configuration.py
@@ -1,9 +1,13 @@
 from enum import Enum, unique
 from pathlib import Path
-from typing import Any
+from typing import Any, Callable, Final
 
 import yaml
 from pydantic import BaseModel, Field
+
+from models import hierarchical_model as hier
+from models import simple_linear_regression as lm
+from models import two_teir_hierarchical as tth
 
 
 @unique
@@ -14,6 +18,28 @@ class Model(Enum):
     SIMPLE_STAN = "SIMPLE_STAN"
     HIERARCHICAL_PYMC3 = "HIERARCHICAL_PYMC3"
     HIERARCHICAL_STAN = "HIERARCHICAL_STAN"
+    TWOTIER_PYMC3 = "TWOTIER_PYMC3"
+    TWOTIER_STAN = "TWOTIER_STAN"
+
+
+ModelCallable = Callable[[str, dict[str, Any]], None]
+
+MODEL_CALLER_MAP: Final[dict[Model, ModelCallable]] = {
+    Model.SIMPLE_PYMC3: lm.simple_pymc3_model,
+    Model.SIMPLE_STAN: lm.simple_stan_model,
+    Model.HIERARCHICAL_PYMC3: hier.hierarchical_pymc3_model,
+    Model.HIERARCHICAL_STAN: hier.hierarchical_stan_model,
+    Model.TWOTIER_STAN: tth.two_tier_hierarchical_stan_model,
+}
+
+
+class ModelNotInCallerLookupMap(BaseException):
+    """Model not in caller lookup map."""
+
+    def __init__(self, model: Model) -> None:
+        msg = f"Need to add '{model.value}' to the lookup dict 'MODEL_CALLER_MAP'."
+        super().__init__(msg)
+        return None
 
 
 class ModelConfiguration(BaseModel):
@@ -30,7 +56,16 @@ class ModelConfigurations(BaseModel):
     configurations: list[ModelConfiguration] = Field(default_factory=list)
 
 
-def read_configuration_file(config_file: Path) -> ModelConfigurations:
+class UnexpectedNumberOfConfigurations(BaseException):
+    """A non-1 number of configurations were located."""
+
+    def __init__(self, n: int, name: str) -> None:
+        msg = f"Found {n} configurations with name '{name}'."
+        super().__init__(msg)
+        return None
+
+
+def _read_configuration_file(config_file: Path) -> ModelConfigurations:
     with open(config_file, "r") as file:
         yaml_data = yaml.safe_load(file)
         configs = ModelConfigurations(
@@ -40,9 +75,38 @@ def read_configuration_file(config_file: Path) -> ModelConfigurations:
 
 
 def get_model_configuration(name: str, config_file: Path) -> ModelConfiguration:
-    configs = read_configuration_file(config_file=config_file)
+    """Get a model configuration from a config file.
+
+    Args:
+        name (str): Name of the configuration.
+        config_file (Path): Path to the configuration file.
+
+    Raises:
+        BaseException: If multiple configurations have the name in the file.
+
+    Returns:
+        ModelConfiguration: Model configuration from the config file.
+    """
+    configs = _read_configuration_file(config_file=config_file)
     config = [c for c in configs.configurations if c.name == name]
     if len(config) == 1:
         return config[0]
     else:
-        raise BaseException(f"Found {len(config)} configurations with name '{name}'.")
+        raise UnexpectedNumberOfConfigurations(n=len(config), name=name)
+
+
+def get_model_callable(mdl_config: ModelConfiguration) -> ModelCallable:
+    """Get a callable function for a model.
+
+    Args:
+        mdl_config (ModelConfiguration): Model configuration.
+
+    Raises:
+        ModelNotInCallerLookupMap: If the requested model is not in the lookup table.
+
+    Returns:
+        ModelCallable: Function to call to fit the model in the configuration.
+    """
+    if mdl_config.model not in MODEL_CALLER_MAP:
+        raise ModelNotInCallerLookupMap(mdl_config.model)
+    return MODEL_CALLER_MAP[mdl_config.model]
